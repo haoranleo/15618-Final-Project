5d3bfb8e6801e80caf7ac5148da10928e797d4b8
haoran.li <lhr449307680@gmail.com>
2020-04-25 16:51:14 -0400

    Added several API needed for testing and implemented constructor and destructor which manages several sentinel nodes

--- src/lock_free_BST.h
+++ src/lock_free_BST.h
@@ -6,13 +6,22 @@
 #define BINARY_SEARCH_TREE_LOCK_FREE_BST_H
 
 #include "BST.h"
+#include "BST_utils.h"
 
 class LockFreeBST : public BinarySearchTree {
 public:
-    LockFreeBST() = default;
+    LockFreeBST() {
+        // Construct the sentinel nodes
+        root_r = new LFTreeNode();
+        root_s = new LFTreeNode();
+        root_t = new LFTreeNode();
+        root_r->right->child = root_s;
+        root_s->right->child = root_t;
+    }
 
-    ~LockFreeBST() = default;
-
+    ~LockFreeBST() override {
+        // TODO: add code to delete all nodes
+    }
 
     /* Traverse the BST to determine whether the given value exists.
      * @param v : Target value to find.
@@ -33,6 +42,28 @@ public:
      *          false if the node doesn't exist or the tree is empty.
      */
     bool remove(int v) override;
+
+    /* Destroy current BST and reconstruct an empty one. */
+    void reinitialize() override;
+
+    /* Write all nodes' value to vector in pre-traversal order
+     * @return : Vector containing all values in the BST in pre-traversal order
+     */
+    vector<int> trans2vec() override;
+
+private:
+    LFTreeNode *root_r, *root_s, *root_t; // They are all sentinel nodes
+
+    /* Destroy current BST
+     * @param cur : Pointer to the current tree node.
+     */
+    void destroy(LFTreeNode* cur);
+
+    /* Helper function to pre-traverse the BST and write values to vector
+     * @param cur : Pointer to the current tree node.
+     * @param v : Vector to store the values in BST.
+     */
+    void trans2vec_helper(LFTreeNode* cur, vector<int>& v);
 };
 
 #endif //BINARY_SEARCH_TREE_LOCK_FREE_BST_H
