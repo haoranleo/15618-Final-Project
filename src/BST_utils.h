af38aea178cbc239269b4763b2a3efaa15e826b9
ZoeZhengYQ <yingqiao.zoe@gmail.com>
2020-04-24 22:48:17 -0400

    Implement data structure needed for lock free BST

--- src/BST_utils.h
+++ src/BST_utils.h
@@ -48,5 +48,74 @@ public:
     Spinlock* lock;     // Sping lock
 };
 
+/********** Structures for lock free BST *************/
+class LFTreeNode {
+public:
+    // Structure for key in lock free BST node
+    struct LFNodeKey {
+        bool modify_flg;  // Indicate whether the value is original one or replaced one
+        int value;  // Actual value of key
+    };
+
+    struct LFNodeChild {
+        // Flags indicate state of the edge between current node and child
+        bool intent_flg;  // mark for intent
+        bool delete_flg;  // mark for delete
+        bool promote_flg;  // mark for promote
+
+        bool null_flg;  // indicate whether the address field contains a null or a non-null value
+
+        LFTreeNode *child;
+    };
+
+    // Data in tree node
+    LFNodeKey key;
+    LFNodeChild* left;
+    LFNodeChild* right;
+    bool ready_to_replace;
+};
+
+// Indicate whether an edge is a left or right edge of parent node
+enum EdgeType { LEFT, RIGHT };
+
+// Structure for lock free tree edges
+struct LFTreeEdge {
+    LFTreeNode *parent;  // Parent node
+    LFTreeNode *child;  // Child node
+    EdgeType type;
+};
+
+// Record for seek phase
+struct SeekRecord {
+    LFTreeEdge last_edge;
+    LFTreeEdge p_last_edge;
+    LFTreeEdge inject_edge;
+};
+
+// Record for anchor node
+struct AnchorRecord {
+    LFTreeNode* node;
+    int key_val;
+};
+
+enum DelMode { INJECT, DISCOVERY, CLEANUP };
+enum DelType { SIMPLE, COMPLEX };
+
+// Record of current execution state
+struct StateRecord {
+    LFTreeEdge target_edge;
+    LFTreeEdge p_target_edge;
+
+    int target_key;
+    int p_target_key;
+
+    DelMode mode;
+    DelType type;
+
+    // the next field stores pointer to a seek record; it is used
+    // for finding the successor if the delete operation is complex
+    SeekRecord *successorRecord;
+};
+
 
 #endif //INC_15618_FINE_GRAINED_BST_UTILS_H
