3ad5876bfe1d8b14b73d0cbfc30f72ee7655bedc
haoran.li <lhr449307680@gmail.com>
2020-04-26 20:08:02 -0400

    Modified test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
-#include "test_fine_grained_BST.h"
+#include "test_BST.h"
+//#include "lock_free_BST.h"
 
 int main() {
-    TestFineGrainedBST* testor = new TestFineGrainedBST();
-//    testor->test_all_basic();
-    testor->test_all_multi(4, 4);
+    TestBST* testor = new TestBST();
+//    testor->test_fine_grained_BST(4, 4);
+    testor->test_lock_free_BST(4, 4);
     delete testor;
+
+//    LockFreeBST* bst = new LockFreeBST();
+//    bst->search(1);
+//    bst->search(2);
+//    bst->insert(1);
+//    bst->insert(3);
+//    bst->insert(2);
+//    bst->insert(2);
+//    bst->insert(2);
+//    bst->search(1);
     return 0;
 }
