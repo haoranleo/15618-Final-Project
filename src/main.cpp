774ba5724fc914f14ab4a939ac99766ce1f12397
haoran.li <lhr449307680@gmail.com>
2020-04-30 19:30:44 -0400

    New test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
-#include "test_BST.h"
+//#include "test_BST.h"
 //#include "lock_free_BST.h"
+#include "benchmark.h"
 
 int main() {
-    TestBST* testor = new TestBST();
+    Benchmark* benchmark = new Benchmark(100);
+//    benchmark->run_fine_grained_BST(10000, 8);
+    benchmark->run_lock_free_BST(10000, 8);
+    delete benchmark;
+
+//    TestBST* testor = new TestBST();
 //    testor->test_fine_grained_BST(4, 4);
-    testor->test_lock_free_BST(4, 4);
-    delete testor;
+//    testor->test_lock_free_BST(4, 4);
+//    delete testor;
 
 //    LockFreeBST* bst = new LockFreeBST();
 //    bst->search(1);
